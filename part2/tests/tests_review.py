34cdd2cff3581f51686516dd5d3b119d61a1a344
Alurae <10475@holbertonstudents.com>
2025-06-17 21:26:36 +0200

    Fix import order in tests_review.py for consistency

--- part2/tests/tests_review.py
+++ part2/tests/tests_review.py
@@ -1,6 +1,8 @@
 import pytest
 import time
+from datetime import datetime
 from app.models.place import Place
 from app.models.user import User
 from app.models.review import Review
 from app.models.amenity import Amenity
+
